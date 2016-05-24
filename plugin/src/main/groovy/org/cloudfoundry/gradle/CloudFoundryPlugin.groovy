a14b61f6c9c8ee8f4ecb0ac29d2c77659f9cb4c1
Rob Winch <rwinch@gopivotal.com>
2016-05-24 11:12:27 -0500

    reproduce issue

---
+++ plugin/src/main/groovy/org/cloudfoundry/gradle/CloudFoundryPlugin.groovy
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2012 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cloudfoundry.gradle
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+import org.cloudfoundry.gradle.tasks.CreateServiceCloudFoundryTask
+
+/**
+ * The main Cloud Foundry plugin class.
+ *
+ * @author Cedric Champeau
+ * @author Scott Frederick
+ */
+class CloudFoundryPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        project.extensions.create("cloudfoundry", CloudFoundryExtension, project)
+        project.cloudfoundry.extensions.services = project.container(CloudFoundryServiceExtension)
+
+        // register tasks
+        createTask(project, 'CreateService', CreateServiceCloudFoundryTask)
+    }
+
+    private void createTask(Project project, String name, Class<? extends DefaultTask> clazz) {
+        project.tasks.create("cf${name}", clazz)
+    }
+}
